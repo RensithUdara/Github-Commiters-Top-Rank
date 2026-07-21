3717c41549d7dc16a82b2b1682cdceafbf31c891
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-07-21 21:39:41 +0530

    Add Committer, GitHubUser interfaces and SortOption type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+export interface Committer {
+  rank: number;
+  username: string;
+  realname?: string;
+  profile: string;
+  commits: number;
+  avatar: string;
+  rankMessage?: string;
+}
+export type Mode = "commits" | "contributions" | "all";
+export interface GitHubUser {
+  login: string;
+  avatar_url: string;
+  name: string | null;
+  bio: string | null;
+  location: string | null;
+  followers: number;
+  following: number;
+  public_repos: number;
+  html_url: string;
+}
+
+export type SortOption =
+  | "alphabetical-asc"
+  | "alphabetical-desc"
+  | "commits-desc"
+  | "commits-asc";
